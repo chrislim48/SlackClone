ab8b37f95fb4c11401777cee153938f915ec49dd
cblim <cblim@ucsc.edu>
2021-09-01 19:28:38 -0700

    adding port and ignore some files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const app = require('./app.js');
 
+const port = process.env.PORT || 3010;
+
 app.listen(3010, () => {
-  console.log(`Server Running on port 3010`);
-  console.log('API Testing UI: http://localhost:3010/v0/api-docs/');
+  console.log(`Server Running on port ${port}`);
+  console.log(`API Testing UI: http://localhost:${port}/v0/api-docs/`);
 });
